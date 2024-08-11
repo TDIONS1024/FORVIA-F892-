9aaa417305e790462fb7ef5f98186baf5466f67f
Travis Singleton <travissingleton714@gmail.com>
2024-08-11 01:59:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ const password_error = document.getElementById ('password_error')
 
 form.addEventListener('log in',(e)=>{
 
+
 if(username.value === ''){
     e.preventDefault();
     username_error.innerHTML="username is required";
+}
+
+if(password.length<6){
+    e.preventDefault();
+    password_error.innerHtml="Must be atleast 6 characters"
 }
 
 
